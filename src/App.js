09f78f0349a45670626326f8b8788ee4d6ceb168
hzc168 <1048224980@qq.com>
2021-07-27 22:45:52 +0800

    添加FileList组件，对FileList进行删改

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import SearchFile from './components/SearchFile'
+import initFiles from './utils/initFiles'
+import FileList from './components/FileList'
 
 // 自定义左侧容器
 let LeftDiv = styled.div.attrs({
@@ -27,8 +29,13 @@ function App() {
                 <LeftDiv>
                     <SearchFile
                         title="我的文档"
-                        onSearch={(value) => {console.log(value)}}
+                        onSearch={(value) => { console.log(value) }}
                     ></SearchFile>
+                    <FileList files={initFiles}
+                        editFile={(id) => console.log(id)}
+                        deleteFile={(id) => console.log("删除：", id)}
+                        saveFile={(id, value) => console.log(id, value) }
+                    ></FileList>
                 </LeftDiv>
                 <RightDiv>右侧</RightDiv>
             </div>
